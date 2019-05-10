1dcee6da20852c64400d01ca84f3e00444039883
George C. Murray <george.c.murray@maine.edu>
2019-05-10 14:03:03 -0400

    chapter_3.0

---
+++ python_tutorials/chapter_3.0.py
@@ -0,0 +1,7 @@
+#this is the first comment
+spam = 1 #and this is the second comment
+         # . . . and now a third1
+text = "# This is not a commend because it's inside quotes."
+
+prefix = 'Py'
+prefix + 'thon'
