f0c1f2bdd38cb10b8325664627004406ea219985
George C. Murray <george.c.murray@maine.edu>
2019-05-11 13:56:06 -0400

    chapter_1

--- r_practice/chapter_1.R
+++ r_practice/chapter_1.R
@@ -39,3 +39,45 @@ x
 y<- c(TRUE, FALSE, TRUE, TRUE)
 summary(x)
 summary(y)
+
+ls()
+str(x)
+
+head(rivers)
+data(rivers)
+str(rivers)
+tail(rivers)
+
+#Cavendish data set.
+require("HistData")
+head(Cavendish)
+str(Cavendish)
+head(Cavendish$density2)
+summary(Cavendish$density2)
+1+2*(3+4)
+4^3 + 3^(2+1)
+sqrt((4+3)*(2+1))
+((1+2)/(3+4))^2
+a<-2
+b<-3
+c<-4
+d<-5
+
+a*b*c*d
+rivers
+exec.pay
+max(exec.pay)
+
+mean(exec.pay, trim=0.1)
+mean(exec.pay, trim=0.1) - mean(exec.pay)
+mean(exec.pay)
+
+orange
+
+require("orange")
+orange
+str(Orange)
+Orange
+
+mean(Orange$age)
+max(Orange$circumference)
