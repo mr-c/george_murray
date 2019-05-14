b39c0e88d2fd0d890c9b6cc9a328b02113e73c72
George C. Murray <george.c.murray@maine.edu>
2019-05-13 22:03:03 -0400

    sentdex_project

---
+++ python_tutorials/sentdex_2.py
@@ -0,0 +1,9 @@
+programming_languages = "Python", "Java", "C++", "C#"
+
+#print(type(programming_languages))
+#comments used to inactivate previously run commands
+
+for language in programming_languages:
+    print(language)
+
+    
---
+++ python_tutorials/sentdex_project.py
@@ -0,0 +1,22 @@
+l = [1,2,3,4,5]
+
+print(l)
+
+l[1]=99
+
+
+
+game = [[0, 0, 0,],
+        [0, 0, 0,],
+        [0, 0, 0,],]
+
+game[0][1] = 1
+
+print("   a  b  c")
+for count, row in enumerate(game):
+    print(count, row)
+
+
+  #for item in row: 
+        #print(item)
+        
