c4a4edb5a8faa681f20a32bd6b26d729292ce0f3
George C. Murray <george.c.murray@maine.edu>
2019-05-22 19:34:17 -0400

    sentdex_python_tutorial

--- python_tutorials/sentdex_project_3.py
+++ python_tutorials/sentdex_project_3.py
@@ -1,6 +1,29 @@
-import itertools
+game_size = 5
+'''
+game = []
 
-'''player_choice = itertools.cycle([1, 2])
+for i in range(game_size):
+    row = []
+    for i in range(game_size):
+        row.append(0)
+    game.append(row)
+'''
+
+game_size = input("What size game of tic tac toe? " )
+game = [[0 for i in range(game_size)] for i in range(game_size)]
+print(game)
+
+
+
+
+
+
+
+
+
+'''import itertools
+
+player_choice = itertools.cycle([1, 2])
 
 for i in range(10):
     print(next(player_choice))
@@ -13,5 +36,26 @@ for i in range(10):
     current_player = choice+1
     print(current_player)
     choice = players[choice]
-    '''
+    
 
+game_size = 3
+print("   0  1  2")
+
+s = "   "+"  ".join([str(i) for i in range(game_size)])
+
+print(s)
+
+dictionaries = {"key1":15, "key2":32}
+
+print(dictionaries["key1"])
+
+dictionaries["hithere"] = 92
+
+print(dictionaries)
+
+
+for i in range(game_size):
+    s += "  "+str(i)
+
+print(s)
+'''
---
+++ python_tutorials/testing.py
@@ -0,0 +1,7 @@
+#looks locally, in standard library location, and third party library installation location
+
+
+#from directory_name.package_name import function as new_function_name
+from package_example_directory.sentdex_package_example import do_a_thing as dat
+
+dat()
