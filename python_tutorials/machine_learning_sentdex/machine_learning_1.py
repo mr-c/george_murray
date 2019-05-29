77038c4986757eade1e5b7f842ca6fe36e67b0f9
George C. Murray <george.c.murray@maine.edu>
2019-05-29 17:23:18 -0400

    machine_learning_tutorial

--- python_tutorials/machine_learning_sentdex/machine_learning_1.py
+++ python_tutorials/machine_learning_sentdex/machine_learning_1.py
@@ -1,7 +1,11 @@
 #loading necessary packages
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np 
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+
 #pulling data from quandl and displaying dataframe
 df = quandl.get('WIKI/GOOGL')
 #reorganizing dataframe to display only information I want
@@ -17,7 +21,23 @@ forecast_col = 'Adj. Close'
 df.fillna('-99999', inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.head())
+
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+#check the documentation for your algorithms, which can be threaded? (n_jobs)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
